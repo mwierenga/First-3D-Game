1959febfcf594d3f3f7c38d9cbe45d625e2e205e
Xgamer911 <Xgamer911@hotmail.com>
2021-09-06 14:17:36 -0500

    Lesson14-Complete

--- First-3D-Game/Assets/Scripts/Enemy.cs
+++ First-3D-Game/Assets/Scripts/Enemy.cs
@@ -20,17 +20,26 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // are we moving to the start?
         if(movingToStart)
         {
+            //overtime move towards the start position
             transform.position = Vector3.MoveTowards(transform.position, startPos, speed * Time.deltaTime);
+
+            //have we reach our target?
             if (transform.position == startPos)
             {
                 movingToStart = false;
             }
         }
+        //are we moving away from the start?
         else
         {
-
+            transform.position = Vector3.MoveTowards(transform.position, startPos + (moveDirection * moveDistance), speed * Time.deltaTime);
+            if (transform.position == startPos+(moveDirection*moveDistance))
+            {
+                movingToStart = true;
+            }
         }
     }
 }
