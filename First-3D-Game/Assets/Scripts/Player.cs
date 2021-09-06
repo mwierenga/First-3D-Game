fcac1adf88897eeba079e3d1ec2869ec05722f61
Xgamer911 <Xgamer911@hotmail.com>
2021-09-06 10:53:18 -0500

    Lesson7-Complete

--- First-3D-Game/Assets/Scripts/Player.cs
+++ First-3D-Game/Assets/Scripts/Player.cs
@@ -10,7 +10,20 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        rig.velocity = new Vector3(x, rig.velocity.y, 0);
+        float x = Input.GetAxis("Horizontal") * moveSpeed;
+        float z = Input.GetAxis("Vertical") * moveSpeed;
+        rig.velocity = new Vector3(x, rig.velocity.y, z);
+
+        Vector3 vel = rig.velocity;
+
+        vel.y = 0;
+
+        if(vel.x != 0 || vel.z != 0)
+        {
+            transform.forward = vel;
+        }
+
+
+
     }
 }
